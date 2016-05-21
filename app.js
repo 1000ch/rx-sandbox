83efdd3e580ccb31a031b4bb47c29368c0556041
1000ch <shogo.sensui@gmail.com>
2016-05-21 17:18:55 +0900

    wip

--- app.js
+++ app.js
@@ -1,30 +1,78 @@
+class Database {
+  constructor(storeName, keyPath) {
+    this.database = null;
+    this.storeName = storeName;
+    this.keyPath = keyPath;
+  }
+
+  get db() {
+    if (!this.database) {
+      let request = indexedDB.open('rx-sandbox', 1);
+
+      return new Promise((resolve, reject) => {
+        request.addEventListener('upgradeneeded', e => {
+          this.database = e.target.result;
+
+          if (this.database.objectStoreNames.contains(this.storeName)) {
+            this.database.deleteObjectStore(this.storeName);
+          }
+
+          this.database.createObjectStore(this.storeName, {
+            keyPath       : this.keyPath,
+            autoIncrement : true
+          });
+
+          resolve(this.database);
+        });
+
+        request.addEventListener('success', e => {
+          this.database = e.target.result;
+
+          resolve(this.database);
+        });
+
+        request.addEventListener('error', e => {
+          reject(e);
+        });
+      });
+    }
+
+    return Promise.resolve(this.database);
+  }
+
+  get() {
+    return new Promise((resolve, reject) => {
+      this.db.then(database => {
+        let transaction = database.transaction([this.storeName], 'read');
+        let store = transaction.objectStore(this.storeName);
+        let range = IDBKeyRange.lowerBound(0);
+        let request = store.openCursor(range);
+        request.addEventListener('success', e => resolve(e));
+        request.addEventListener('error', e => reject(e));
+      });
+    });
+  }
+
+  put(data) {
+    return new Promise((resolve, reject) => {
+      this.db.then(database => {
+        let transaction = database.transaction([this.storeName], 'readwrite');
+        let store = transaction.objectStore(this.storeName);
+        transaction.addEventListener('complete', () => resolve());
+        transaction.addEventListener('error', e => reject(e));
+        store.put(data);
+      });
+    });
+  }
+}
+
 const domready = Rx.Observable.fromEvent(document, 'DOMContentLoaded');
 
 domready.subscribe(e => {
-  const request = indexedDB.open('rx-sandbox', 1);
-
-  Rx.Observable.fromEvent(request, 'upgradeneeded')
-    .pluck('target', 'result')
-    .subscribe(db => {
-      if (db.objectStoreNames.contains('todo')) {
-        db.deleteObjectStore('todo');
-      }
-
-      db.createObjectStore('todo', {
-        keyPath       : 'datetime',
-        autoIncrement : true
-      });
-    });
-
-  Rx.Observable.fromEvent(request, 'success')
-    .pluck('target', 'result')
-    .subscribe(db => {
-      console.log(db);
-    });
-
   const input = document.querySelector('input');
   const ul = document.querySelector('ul');
   const list = ul.getElementsByTagName('li');
+  const database = new Database('todo', 'timestamp');
 
   Rx.Observable.fromEvent(input, 'keyup')
     .debounce(100)
@@ -32,8 +80,15 @@ domready.subscribe(e => {
     .filter(e => e.shiftKey)
     .pluck('target', 'value')
     .subscribe(value => {
-      let li = document.createElement('li');
-      li.textContent = value;
-      ul.appendChild(li);
+      let data = {
+        text      : value,
+        timestamp : Date.now()
+      };
+
+      database.put(data).then(() => {
+        let li = document.createElement('li');
+        li.textContent = data.text;
+        ul.appendChild(li);
+      });
     });
 });
